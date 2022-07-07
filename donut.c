82d239eab7098e0cd6be862ef0d48c39a94e621b
d4rsen <darsenungarlinov00@gmail.com>
2022-07-07 09:42:57 +0300

    math main

--- donut.c
+++ donut.c
@@ -2,4 +2,26 @@
 #include <string.h>
 
 int k;
-double sin(), cos()
+double sin(), cos();
+
+int main() {
+  float A = 0, B = 0, i, j, z[1760];
+  char b[1760];
+  printf("\nb2J");
+  for (;;){
+    memset(b, 32, 1760);
+    memset(z, 0, 7040);
+
+    for(j = 0, 6.28 > j, j += 0.01) {
+      for(i = 0, 6.28 > i, i += 0.09) {
+        float c = sin(i), d = cos(j), e = sin(A), f = sin(j), g = cos(A),
+          h = d + 2, D = 1 / (c * h * e + f * g + 5), l = cos(i),
+          m = cos(B), n = sin(B),
+          t = c * h * g - f * c;
+
+          int x = 40 + 30 * D * (l * h * m - t * n);
+          
+      }
+    }
+  }
+}
